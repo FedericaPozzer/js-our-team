631c242e82c475922c43e2873de7ae4ca6161cbc
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-06 14:46:16 +0100

    init project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Hi Kik");
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1: stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2: stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// Wayne Barnett		Founder & CEO			wayne - barnett - founder - ceo.jpg
+// Angela Caroll		Chief Editor			angela - caroll - chief - editor.jpg
+// Walter Gordon		Office Manager			walter - gordon - office - manager.jpg
+// Angela Lopez		Social Media Manager	angela - lopez - social - media - manager.jpg
+// Scott Estrada		Developer				scott - estrada - developer.jpg
+// Barbara Ramos		Graphic Designer		barbara - ramos - graphic - designer.jpg
